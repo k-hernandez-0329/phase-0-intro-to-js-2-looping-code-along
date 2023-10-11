f420f035212dc839851531c569edd7f5c252b9a8
karenhernandez <khernandez0329@gmail.com>
2023-10-11 15:35:52 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(cards){
+    for (let a = 0; a < cards.length; a++){
+        cards[a] = `Thank you, ${cards[a]}, for the wonderful surprise gift!`
+    }
+    return cards
+   
+}
+console.log(writeCards(cards))
+
+function countDown(){
+    let countDown = 0
+while (countDown < 11){
+    console.log(countDown++)
+}
+}
